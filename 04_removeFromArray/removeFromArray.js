38553e5ce9ed771c6ea3cdd0048b0e329a1e2506
b4yerl <gcbayerlc@gmail.com>
2022-08-22 17:59:44 -0300

    JS Exercicio 04 resolvido

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(fullArray) {
+    let index = 0;
+    for (let i = 1; i < arguments.length; i++) {
+        if (fullArray.includes(arguments[i])){
+        index = fullArray.indexOf(arguments[i])
+        fullArray.splice(index, 1);
+        }
+    }
+    return fullArray;
 };
 
 // Do not edit below this line
