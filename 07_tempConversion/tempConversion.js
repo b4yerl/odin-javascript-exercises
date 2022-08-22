33abf6c8efbd971275ada9e03b47b4101d5b3fae
b4yerl <gcbayerlc@gmail.com>
2022-08-22 19:12:28 -0300

    JS Exercicio 06 resolvido

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  //c=(f-32)*(5/9)
+  let c = (f - 32) * (5 / 9)
+  return c % 1 == 0 ? c : parseFloat(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  //f=c*(5/9)+32
+  let f = c * (9 / 5) + 32;
+  return f % 1 == 0 ? f : parseFloat(f.toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
