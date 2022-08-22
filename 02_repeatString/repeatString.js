97982e712be104e7559997fda6c4de4bfe2667b6
b4yerl <gcbayerlc@gmail.com>
2022-08-22 16:57:58 -0300

    JS Exercicio 02 resolvido

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    let concatString = '';
+    for (let i = 0; i < n; i++) {
+        concatString = concatString + string;
+    }
+    return concatString;
 };
 
 // Do not edit below this line
