cdb119ea383389ed2871f5bacb3d228b25210ed4
b4yerl <gcbayerlc@gmail.com>
2022-08-22 18:15:08 -0300

    JS Exercicio 05 resolvido

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(i, j) {
+    if (i < 0 || j < 0 || typeof i != 'number' || typeof j != 'number') {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    if (i <= j){ 
+        do {
+            sum += i;
+            i++;
+        } while (i <= j);
+    }
+    else {
+        do {
+            sum += j;
+            j++;
+        } while (j <= i);
+    }
+    return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
